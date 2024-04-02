323bddad32cb39acf1c895207042f18c54f3c2b2
Olesya <pachevaol@gmail.com>
2024-04-02 12:29:55 +0300

    dropdown menu from site

--- main.js
+++ main.js
@@ -1,21 +1,2 @@
 import "./styles/main.scss";
 //---------------------переключения языка---------------------------//
-
-document.addEventListener("DOMContentLoaded", function () {
-  const languageSwitcherBtn = document.getElementById("language-switcher-btn");
-  const dropdown = document.querySelector(".dropdown");
-
-  languageSwitcherBtn.addEventListener("click", function () {
-    dropdown.classList.toggle("show");
-  });
-
-  // Закрытие выпадающего списка при клике вне его области
-  document.addEventListener("click", function (event) {
-    if (
-      !dropdown.contains(event.target) &&
-      event.target !== languageSwitcherBtn
-    ) {
-      dropdown.classList.remove("show");
-    }
-  });
-});
